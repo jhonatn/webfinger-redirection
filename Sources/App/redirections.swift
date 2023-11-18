ee287751b85b1e668037ca298b69d54244ea131c
Jhonatan A <1548170+jhonatn@users.noreply.github.com>
2023-11-17 22:27:47 -0500

    My redirections

--- Sources/App/redirections.swift
+++ Sources/App/redirections.swift
@@ -1,2 +1,14 @@
 let redirections: [String : WebFingerRedirectionDestination] = [
+    "dev": .init(
+        user: "mr_io",
+        domain: "mastodon.social"
+    ),
+    "me": .init(
+        user: "mr_io",
+        domain: "mastodon.social"
+    ),
+    "nazca": .init(
+        user: "nazca_app",
+        domain: "mastodon.social"
+    )
 ]
